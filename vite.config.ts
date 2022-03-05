654e3dfd90b03d4a7eef204bc7492056af11b519
Steven Bennett <steven@budgetbloom.com>
2022-03-05 07:30:38 -0800

    Ye

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     rollupOptions: {
       // Externalize deps that shouldn't be bundled
       external: [
+        'aws-sdk',
         'react',
         'react-dom',
         'react-redux',
