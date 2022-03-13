376441fb67b0b5a3e476b19d6620d0331e8fbfed
Steven Bennett <steven@budgetbloom.com>
2022-03-13 13:12:17 -0700

    Add external dep

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     rollupOptions: {
       // Externalize deps that shouldn't be bundled
       external: [
+        'aws-sdk',
         // '@reduxjs/toolkit',
         // '@srclaunch/exceptions',
         // '@srclaunch/http-client',
