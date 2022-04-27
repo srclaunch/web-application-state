42caac79f4fb31162dcb0c8759e297f13b9d30f7
Steven Bennett <steven@budgetbloom.com>
2022-04-26 19:37:42 -0700

    Update gitignore

--- .srclaunch/config.js
+++ .srclaunch/config.js
@@ -12,6 +12,18 @@ export default {
   description: 'Redux state and utilities used by AppLab web applications',
   type: ProjectType.Library,
   build: {
+    external: [
+      '@srclaunch/exceptions',
+      '@srclaunch/logger',
+      '@reduxjs/toolkit',
+      'react',
+      'react-dom',
+      'react-redux',
+    ],
+    input: {
+      directory: 'src',
+      file: 'index.tsx',
+    },
     formats: [BuildFormat.ESM, BuildFormat.UMD],
     platform: BuildPlatform.Browser,
     target: BuildTarget.ESNext,
